345a4782898b477572cb4675a475da5961fcf571
develop4God <develop4God@gmail.com>
2026-03-15 20:39:17 -0500

    Initialize component sizes in AnimalComponent and ArkComponent

--- lib/components/animals/animal_component.dart
+++ lib/components/animals/animal_component.dart
@@ -1,21 +1,27 @@
 import 'package:biblitos/components/animals/animal_config.dart';
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
+import 'package:flutter/material.dart';
 
 class AnimalComponent extends SpriteComponent with TapCallbacks {
   final AnimalConfig config;
   final void Function(String audioKey, String reactAnimation) onTapped;
+  late Vector2 _initialSize;
 
   AnimalComponent({
     required this.config,
     required this.onTapped,
     required Vector2 position,
     required Vector2 size,
-  }) : super(position: position, size: size);
+  }) : super(position: position, size: size) {
+    _initialSize = size;
+  }
 
   @override
   Future<void> onLoad() async {
     sprite = await Sprite.load(config.spritePath);
+    size = _initialSize;
+    debugPrint('🧩 ${config.animalKey} size set to $size');
   }
 
   @override
--- lib/components/props/ark_component.dart
+++ lib/components/props/ark_component.dart
@@ -4,18 +4,22 @@ import 'package:flutter/material.dart';
 
 class ArkComponent extends SpriteComponent with TapCallbacks {
   final void Function() onTapped;
+  late Vector2 _initialSize;
 
   ArkComponent({
     required this.onTapped,
     required Vector2 position,
     required Vector2 size,
-  }) : super(position: position, size: size);
+  }) : super(position: position, size: size) {
+    _initialSize = size;
+  }
 
   @override
   Future<void> onLoad() async {
     debugPrint('🧩 ArkComponent loading sprite');
     sprite = await Sprite.load('noah_ark/props/ark.png');
-    debugPrint('🧩 ArkComponent sprite loaded — size: $size');
+    size = _initialSize;
+    debugPrint('🧩 Ark size set to $size');
   }
 
   @override
